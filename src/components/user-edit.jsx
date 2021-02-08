04353c49397d21428448cc8f0b4a22e74baf799f
Esaúl <f.esaul.franco.r@gmail.com>
2021-02-08 16:05:59 -0600

    fix

--- src/components/user-edit.jsx
+++ src/components/user-edit.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import * as actions from "../actions";
 import { connect } from "react-redux";
 import { reduxForm, Field, change } from "redux-form";
 import { Input } from "@material-ui/core";
